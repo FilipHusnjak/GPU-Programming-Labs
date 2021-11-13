7ad7cf586f4e32ec5cb2c2e9482d6794ad05fb71
FilipHusnjak <filip.husnjak@fer.hr>
2021-11-13 16:17:13 +0100

    Implement exercise 1

---
+++ lab-1/exercise_1.cu
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define TPB 256
+#define BDIMX 1 
+
+__global__ void helloWorld()
+{
+	printf("Hello World! My threadId is %d\n", threadIdx.x);
+}
+
+int main()
+{
+	helloWorld<<<dim3(BDIMX), dim3(TPB)>>>();
+
+	cudaDeviceSynchronize();
+
+	return 0;
+}
